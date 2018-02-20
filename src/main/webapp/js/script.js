65ad3141eeaeeb22bf750168f8642c4cbe698fb2
Tanuj <tanujd@crosstab.in>
2018-02-20 12:36:19 +0530

    slitSliderCommit

--- src/main/webapp/js/script.js
+++ src/main/webapp/js/script.js
@@ -1,4 +1,15 @@
+$("#fakeLoader").fakeLoader({
 
+	timeToHide : 1200, // Time in milliseconds for fakeLoader disappear
+
+	zIndex : "1001",// Default zIndex
+
+	spinner : "spinner1",// Options: 'spinner1', 'spinner2', 'spinner3',
+	// 'spinner4', 'spinner5', 'spinner6', 'spinner7'
+
+	bgColor : "#4286f4", // Hex, RGB or RGBA colors
+
+});
 $(document).ready(function() {
 	$("#testimonial-slider").owlCarousel({
 		items : 3,
